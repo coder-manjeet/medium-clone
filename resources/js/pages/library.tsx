7bce3c7c5d81f2ad61dd92302b7cd485f3ff4641
coder-manjeet <codermanjeet@gmail.com>
2026-01-20 23:21:13 +0530

    dummy pages added

---
+++ resources/js/pages/library.tsx
@@ -0,0 +1,34 @@
+import { Head } from '@inertiajs/react';
+
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import { library } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Library',
+        href: library().url,
+    },
+];
+
+export default function Library() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Library" />
+            <div className="flex h-[80vh] flex-1 flex-col gap-4 overflow-x-auto p-4">
+                <div className="relative grid auto-rows-min gap-4 md:grid-cols-100 overflow-hidden ">
+                    <div className="relative h-[80vh] w-auto overflow-hidden dark:border-sidebar-border start col-span-16">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="relative h-[80vh] w-auto overflow-hidden dark:border-sidebar-border start col-span-50">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="relative overflow-hidden dark:border-sidebar-border start col-span-34">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
---
+++ resources/js/pages/profile.tsx
@@ -0,0 +1,34 @@
+import { Head } from '@inertiajs/react';
+
+import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import AppLayout from '@/layouts/app-layout';
+import { profile } from '@/routes';
+import { type BreadcrumbItem } from '@/types';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Profile',
+        href: profile().url,
+    },
+];
+
+export default function Profile() {
+    return (
+        <AppLayout breadcrumbs={breadcrumbs}>
+            <Head title="Profile" />
+            <div className="flex h-[80vh] flex-1 flex-col gap-4 overflow-x-auto p-4">
+                <div className="relative grid auto-rows-min gap-4 md:grid-cols-100 overflow-hidden ">
+                    <div className="relative h-[80vh] w-auto overflow-hidden dark:border-sidebar-border start col-span-16">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="relative h-[80vh] w-auto overflow-hidden dark:border-sidebar-border start col-span-50">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                    <div className="relative overflow-hidden dark:border-sidebar-border start col-span-34">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                    </div>
+                </div>
+            </div>
+        </AppLayout>
+    );
+}
